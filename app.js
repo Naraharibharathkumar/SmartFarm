1a81bd4edae7b47d717f90420b9f48b549904a45
Bharath Kumar Narahari <Naraharibharathkumar@gmail.com>
2016-05-02 12:20:29 -0700

    resolving merge conflicts

--- app.js
+++ app.js
@@ -7,10 +7,28 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var apiCalls = require('./routes/apiCalls');
 
 var app = express();
 
+/* PLEASE UNCOMMENT ONLY WHILE USING MONGODB WITH THIS APP*/
+/*
+var home = require('./routes/home');
+var mongoSessionConnectURL = "mongodb://localhost:27017/smartfarm";
+var expressSession = require("express-session");
+var mongoStore = require("connect-mongo")(expressSession);
+var mongo = require("./routes/mongo");
+
+app.use(expressSession({
+  secret: 'cmpe272_team9',
+  resave: false,  //don't save session if unmodified
+  saveUninitialized: false,	// don't create session until something stored
+  duration: 30 * 60 * 1000,
+  activeDuration: 5 * 60 * 1000,
+  store: new mongoStore({
+    url: mongoSessionConnectURL
+  })
+}));
+*/
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -25,8 +43,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.get('/getData', apiCalls.getData);
-
+//app.get('/dbconnection',home.checkDBConnection);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
---
+++ routes/home.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Tiya on 29-04-2016.
+ */
+
+var mongo = require("./mongo");
+var mongoURL = "mongodb://localhost:27017/smartfarm";
+
+exports.checkDBConnection = function(req,res){
+
+
+    mongo.connect(mongoURL, function() {
+        console.log('Connected to mongo at: ' + mongoURL);
+        var coll = mongo.collection('smartfarm');
+        //database connection testing with static values
+        coll.find({CropName: "Barley"}).toArray(function(err, user) {
+
+        if(err){
+            throw err;
+        }
+        else{
+            if(user )
+            {
+                var price=user[0].Price_2015;
+                console.log("database connection is working fine"+price);
+                res.render('/');
+            }
+            else{
+
+                console.log("could not get data");
+                res.render('/');
+            }
+
+        }
+        });
+    });
+
+};
+    
