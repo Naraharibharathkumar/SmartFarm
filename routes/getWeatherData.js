63cf09bfff5a3c6cedc91edabe3c7cdde3c80ac4
Bharath Kumar Narahari <Naraharibharathkumar@gmail.com>
2016-05-02 12:17:07 -0700

    Uploaded the getData call for getting data from NASS Quickstats - Raw Version

---
+++ routes/getWeatherData.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Bharath Kumar on 4/26/2016.
+ */
+var weatherCreds = {
+    "username": "d5f4b7d0-9527-4957-9245-ff084fb0c751",
+        "password": "EtlPJuODpr",
+    "host": "twcservice.mybluemix.net",
+    "port": 443,
+    "url": "https://d5f4b7d0-9527-4957-9245-ff084fb0c751:EtlPJuODpr@twcservice.mybluemix.net"
+}
